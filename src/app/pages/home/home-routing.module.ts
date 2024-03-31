15ca3020216714b0e0fe4afb7e058d1c7b6818cc
Layr <layrfpf@gmail.com>
2024-03-31 17:43:35 -0300

    notification routing works (child routing logic)

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { NotificationsPage } from './notifications/notifications.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
     pathMatch: 'full'
+  },
+  {
+    path: 'notificacoes',
+    component: NotificationsPage
   }
 ];
 
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NotificationsPage } from './notifications/notifications.page';
 
 @NgModule({
   imports: [
     SharedModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, NotificationsPage]
 })
 export class HomePageModule {}
