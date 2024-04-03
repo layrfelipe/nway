8e0b3a7cb87845bb36b4ac35fa74ebc535e3f364
Layr <layrfpf@gmail.com>
2024-04-02 21:51:32 -0300

    specific customer details page

--- src/app/pages/customers/customers-routing.module.ts
+++ src/app/pages/customers/customers-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomersPage } from './customers.page';
+import { CustomerPage } from './customer/customer.page';
 
 const routes: Routes = [
   {
     path: '',
     component: CustomersPage,
     pathMatch: 'full'
+  },
+  {
+    path: 'cliente',
+    component: CustomerPage
   }
 ];
 
--- src/app/pages/customers/customers.module.ts
+++ src/app/pages/customers/customers.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CustomersPage } from './customers.page';
 import { CustomersPageRoutingModule } from './customers-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CustomerPage } from './customer/customer.page';
 
 @NgModule({
   imports: [
     SharedModule,
     CustomersPageRoutingModule
   ],
-  declarations: [CustomersPage]
+  declarations: [CustomersPage, CustomerPage]
 })
 export class CustomersPageModule {}
