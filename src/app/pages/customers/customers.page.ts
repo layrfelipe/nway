33378472477bc0ff8b3ff9b76cc1f4592a72776f
Layr <layrfpf@gmail.com>
2024-04-03 02:49:57 -0300

    customers responsive (not ok yet)

--- src/app/pages/customers/customers.page.ts
+++ src/app/pages/customers/customers.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['customers.page.scss'],
 })
 export class CustomersPage {
+
+  protected screenWidth = signal<number>(0)
+  protected isMobile = computed(() => {
+    return this.screenWidth() < 993
+  })
 
   constructor(
     private router: Router
