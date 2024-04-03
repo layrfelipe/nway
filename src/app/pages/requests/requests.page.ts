2459536adbb027edca3f412781909af1fb8f5ce1
Layr <layrfpf@gmail.com>
2024-04-03 02:07:13 -0300

    requests page responsive

--- src/app/pages/requests/requests.page.ts
+++ src/app/pages/requests/requests.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,11 +6,20 @@ import { Router } from '@angular/router';
   templateUrl: 'requests.page.html',
   styleUrls: ['requests.page.scss'],
 })
-export class RequestsPage {
+export class RequestsPage implements OnInit {
+
+  protected screenWidth = signal<number>(0)
+  protected isMobile = computed(() => {
+    return this.screenWidth() < 993
+  })
 
   constructor(
     private router: Router
   ) {}
+
+  ngOnInit() {
+    this.screenWidth.set(window.innerWidth);
+  }
 
   goToRequestStatusPage() {
     this.router.navigate(['solicitacoes', 'status'])
