ba9c080a008b2846142f85fa07de2ac6bd050ae4
Layr <layrfpf@gmail.com>
2024-04-04 02:02:21 -0300

    measurement query by client page responsive

--- src/app/pages/measurement-query/customer/measurement-query-customer.page.ts
+++ src/app/pages/measurement-query/customer/measurement-query-customer.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-measurement-query-customer',
   templateUrl: 'measurement-query-customer.page.html',
   styleUrls: ['measurement-query-customer.page.scss'],
 })
-export class MeasurementQueryCustomerPage {
+export class MeasurementQueryCustomerPage implements OnInit {
+
+  protected screenWidth = signal<number>(0)
+
+  protected isPhone = computed(() => {
+    return this.screenWidth() < 768
+  })
 
   constructor(
   ) {}
+
+  ngOnInit(): void {
+    this.screenWidth.set(window.innerWidth)
+  }
 }
