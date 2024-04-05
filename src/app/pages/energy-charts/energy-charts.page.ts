8d82e5ff5b0764251cd2f8adc7042da70b8374d2
Layr <layrfpf@gmail.com>
2024-04-05 12:36:14 -0300

    charts

--- src/app/pages/energy-charts/energy-charts.page.ts
+++ src/app/pages/energy-charts/energy-charts.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-energy-charts',
@@ -6,7 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['energy-charts.page.scss'],
 })
 export class EnergyChartsPage {
+  @ViewChild('lineCanvas') protected lineCanvas: any;
+  lineChart: any;
 
   constructor(
   ) {}
+
+  ngAfterViewInit() {
+    this.lineChartMethod();  
+  }
+
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        datasets: [
+          {
+            label: 'Consumo em kWh',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [200, 230, 150, 650, 610, 620, 200, 420, 100, 450],
+            spanGaps: false,
+          },
+          {
+            label: 'Consumo em kWh',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [100, 130, 180, 620, 800, 400, 476, 300, 120, 400],
+            spanGaps: false,
+          }
+        ]
+      }
+    })
+  }
 }
